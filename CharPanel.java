002c82445253932366239b0e00aa7f1e8547b95a
Sarah D. Perea <sdperea1@up.edu.ph>
2016-11-13 17:26:49 +0800

    Fixed errors on classes for moving avatars

--- CharPanel.java
+++ CharPanel.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 
+
+//A class for drawing the avatar
 public class CharPanel extends JPanel {
 		ImageIcon img;	
 
@@ -8,9 +10,11 @@ public class CharPanel extends JPanel {
 			this.img = new ImageIcon(this.getClass().getResource("fish.png"));
 
 		}
+		
+		//draws the avatar to the panel
 		public void paintComponent(Graphics g) {
 
-			super.paintComponent(g);
+			super.paintComponent(g); 
 		  img.paintIcon(this, g, 0,0);
 		}
 
